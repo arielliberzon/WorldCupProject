9855a588c931934d3aa31c390697d877fa501865
arielliberzon <ariellibe@gmail.com>
2021-04-21 17:52:32 -0400

    Simple tester class added

---
+++ src/Tester.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Tester {
+    public static void main(String[] args) throws IOException {
+        TeamInfo info = new TeamInfo();
+        System.out.println(info.getTeam("BEL"));
+    }
+}
