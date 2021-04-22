4f3013377ed46f9bb2d4d81179d8ca8fb2eca7b6
HarjitHXS <80358565+HarjitHXS@users.noreply.github.com>
2021-04-22 00:16:48 -0400

    comment to read

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
 
 import javafx.application.Application;
-
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
 import javafx.scene.layout.*;
 
-import javafx.scene.paint.Color;
+
 import javafx.stage.Stage;
 
 public class Main extends Application {
     private final StackPane root = new StackPane();
     private final BorderPane r = new BorderPane();
+    private final GridPane masterpane = new GridPane();
     private Scene scene = new Scene(root);
-    private Button play = new Button("play");
+    private Button play = new Button("    play   ");// spacing it out for now add iamge later
 
 
 
@@ -45,14 +44,14 @@ public class Main extends Application {
         n.getTabs().addAll(qualifierStageTab,groupStageTab,knockoutStageTab);
         return n;
     }
-
+//all the scene and pane are base on this method add and remove as you like
     private void showIntroScene(Stage window) {
-        GridPane masterpane = new GridPane();
         Image img = new Image("sc.jpg");
         masterpane.setBackground(new Background(new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
                 BackgroundSize.DEFAULT)));
+        play.setStyle("-fx-background-color: BLUE");//just color for now adding image later
         masterpane.add(play, 0, 1);
-        play.setTranslateX(700);
+        play.setTranslateX(800);
         play.setTranslateY(500);
         r.setTop(topping());
         r.setTranslateY(50);
