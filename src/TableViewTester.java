8d3f70387ff8960c1105aacbcc7d8b781735e9c1
arielliberzon <ariellibe@gmail.com>
2021-04-22 16:23:24 -0400

    Added table view

---
+++ src/TableViewTester.java
@@ -0,0 +1,52 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TableViewTester extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Load all the teams by creating a TeamInfo object
+        TeamInfo teamInfo = new TeamInfo();
+
+        // Create a TableView with a list of persons
+        TableView<Team> table = new TableView<>();
+        // Add rows to the TableView
+        table.getItems().addAll(TableViewHelper.getTeamList(teamInfo.getTeamMap()));
+        // Add columns to the TableView
+        table.getColumns().addAll(TableViewHelper.getTeamRankingColumn(), TableViewHelper.getCountryNameColumn(),
+                TableViewHelper.getTotalPointsColumn(), TableViewHelper.getCodeColumn());
+
+        // Set the column resize policy to constrained resize policy
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        // Set the Placeholder for an empty table
+        table.setPlaceholder(new Label("No visible columns and/or data exist."));
+
+        // Create the VBox
+        VBox root = new VBox();
+        // Add the Table to the VBox
+        root.getChildren().add(table);
+        // Set the Padding and Border for the VBox
+        root.setStyle("-fx-padding: 10;" +
+                "-fx-border-style: solid inside;" +
+                "-fx-border-width: 2;" +
+                "-fx-border-insets: 5;" +
+                "-fx-border-radius: 5;" +
+                "-fx-border-color: blue;");
+
+        // Create the Scene
+        Scene scene = new Scene(root);
+        // Add the Scene to the Stage
+        stage.setScene(scene);
+        // Set the Title of the Stage
+        stage.setTitle("A simple TableView Example");
+        // Display the Stage
+        stage.show();
+    }
+}
