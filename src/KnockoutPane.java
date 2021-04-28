e76a31789caeef01bf8bb30fd798cd8db9296716
greenwolf5 <25jazz25@gmail.com>
2021-04-27 23:13:21 -0400

    Update KnockoutPane.java
    Oops, forgot to do the values that look better for everyone else

--- src/KnockoutPane.java
+++ src/KnockoutPane.java
@@ -32,9 +32,9 @@ public class KnockoutPane extends Pane {
     }
 
     private void createBracket(Simulator sim){
-        int x = 100;          // Z.L. (modified value in "x" variable from 100 to 120.)      //The initial X cord of the top left bracket;
+        int x = 120;          // Z.L. (modified value in "x" variable from 100 to 120.)      //The initial X cord of the top left bracket;
         int y = 50;                              //The initial y cord of the top left bracket;
-        int scalingFactor = 50;      // Z.L. (modified value in "scalingFactor" from 50 to 40.)   //Scaling factor of the bracket, increase = bigger
+        int scalingFactor = 40;      // Z.L. (modified value in "scalingFactor" from 50 to 40.)   //Scaling factor of the bracket, increase = bigger
         int yLength = scalingFactor/2;          //The length of the line going up and down on the L shape. **MIGHT GET REMOVED / TWEAKED since the scaling doesn't 100% fit with it.**
         int horizontalLength = scalingFactor*4;            //The length of the line going left and right on the L shape.  **MIGHT GET REMOVED // TWEAKED since the scaling isn't 100% with it.**
         int yIncrement = scalingFactor*2;       //How much will the next tier bracket be moved down. 
