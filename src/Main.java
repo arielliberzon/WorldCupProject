1d4af2132123cf61caf59016eada8af302d348c5
arielliberzon <ariellibe@gmail.com>
2021-04-29 13:39:49 -0400

    tinkering branch

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,23 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
     private final StackPane root = new StackPane();
     private final BorderPane r = new BorderPane();
-    private final GridPane masterpane = new GridPane();
+    private final GridPane masterPane = new GridPane();
     private Scene scene = new Scene(root);
     private Button play;// spacing it out for now add image later
-    private TeamInfo info = new TeamInfo();
-    // TODO: ADD buttons to simulate; currently hardcoded when running
+    //private TeamInfo info = new TeamInfo();
+
+    private QualifierPane qualifierPane;
+    private GroupStage groupStage;
+    private KnockoutPane knockoutPane;
+    private TabPane tabPane;
+
     private Simulator simulator = new Simulator();
 
     public Main() throws IOException {
@@ -33,40 +34,55 @@ public class Main extends Application {
         launch(args);
     }
 
-    public TabPane topping(Double height, Double width){
+    private void setTopping(Double height, Double width){
 
         // HARDCODED QUALIFIERS//
-        QualifierPane qualifierPane = new QualifierPane(simulator.getTeamMap(), height, width);
+        qualifierPane = new QualifierPane(simulator.getTeamMap(), height, width);
+        groupStage = new GroupStage(height, width, simulator);
+        knockoutPane = new KnockoutPane(simulator);
+
         qualifierPane.updateTeamMap(simulator.getQualifiedTeams());
         simulator.simulateGroups();
         // END OF HARDCODED TEST //
 
-        TabPane n = new TabPane();
-        Tab groupStageTab = new Tab("   Group Stage   ", new GroupStage(height, width, simulator));
-        Tab knockoutStageTab = new Tab("   Knockout Stage  ",new KnockoutPane(simulator));
+        tabPane = new TabPane();
+        Tab groupStageTab = new Tab("   Group Stage   ", groupStage);
+        Tab knockoutStageTab = new Tab("   Knockout Stage  ", knockoutPane);
         Tab qualifierStageTab = new Tab("   Teams   ", qualifierPane);
 
 
         groupStageTab.setClosable(false);
         knockoutStageTab.setClosable(false);
         qualifierStageTab.setClosable(false);
-        n.getTabs().addAll(qualifierStageTab,groupStageTab,knockoutStageTab);
-        return n;
+        tabPane.getTabs().addAll(qualifierStageTab,groupStageTab,knockoutStageTab);
     }
-//all the scene and pane are base on this method add and remove as you like
+
+    private TabPane getTopping() {
+        return tabPane;
+    }
+
+    private HBox getButtonBar() {
+        HBox hBox = new HBox();
+        Button simulateAll = new Button("Simulate Everything");
+        hBox.getChildren().addAll(hBox);
+        return hBox;
+    }
+
+    //all the scene and pane are base on this method add and remove as you like
     private void showIntroScene(Stage window) {
         Image img = new Image("Images/background.jpg");
-        masterpane.setBackground(new Background(new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+        masterPane.setBackground(new Background(new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
         play = new Button("Start>>");
         play.setStyle("-fx-background-color: LIGHTGREY");
-        masterpane.add(play, 0, 1);
+        masterPane.add(play, 0, 1);
         play.setTranslateX(800);
         play.setTranslateY(500);
-        window.setScene(new Scene(masterpane));
+        window.setScene(new Scene(masterPane));
         window.setMaximized(true);
         window.show();
 
-        r.setTop(topping(window.getHeight(), window.getWidth()));
+        setTopping(window.getHeight(), window.getWidth());
+        r.setTop(getTopping());
         r.setTranslateY(50);
         root.getChildren().add(r);
         play.setOnAction(e -> window.setScene(scene));
