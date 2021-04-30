206f85eba8523037a69a91995377b004980d1b5f
arielliberzon <ariellibe@gmail.com>
2021-04-29 23:01:22 -0400

    Added tooltip for each conference with pictures!

--- src/QualifierPane.java
+++ src/QualifierPane.java
@@ -1,15 +1,19 @@
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 
 public class QualifierPane extends BorderPane {
 
     private Map<String, Team> teamMap;
+    private Map<String, String> confInfo;
     private TableView table;
     private HBox topButtonBar;
     private Button allButton;
@@ -24,14 +28,16 @@ public class QualifierPane extends BorderPane {
     private TextField searchField;
 
 
-    public QualifierPane( Double height, Double width,Simulator simulator) {
+    public QualifierPane(Double height, Double width,Simulator simulator) {
+        confInfo = new HashMap<>();
+
         teamMap = simulator.getTeamMap();
         updateTeamMap(simulator.getQualifiedTeams());
 
         createTable();
-        //table.setMaxSize(width- 50, height);
         setCenter(table);
 
+        createConfMap();
         createButtonBar();
         setTop(topButtonBar);
 
@@ -39,6 +45,15 @@ public class QualifierPane extends BorderPane {
         this.setMinWidth(width);
     }
 
+    private void createConfMap() {
+        //HashMap<String,String> confInfo = new HashMap<>();
+        confInfo.put("UEFA", "EUROPE\n\n55 TEAMS\n\n13 PLACES");
+        confInfo.put("CONMEBOL", "SOUTH AMERICA\n\n10 TEAMS\n\n4.5 PLACES");
+        confInfo.put("CONCACAF", "NORTH AMERICA, CENTRAL AMERICA, CARIBBEAN\n\n35 TEAMS\n\n3.5 PLACES");
+        confInfo.put("CAF", "AFRICA\n\n53 TEAMS\n\n5 PLACES");
+        confInfo.put("AFC", "ASIA\n\n46 TEAMS\n\n4.5 PLACES");
+        confInfo.put("OFC", "OCEANIA\n\n9 TEAMS\n\n0.5 PLACES");
+    }
 
 
     private void createButtonBar() {
@@ -51,6 +66,7 @@ public class QualifierPane extends BorderPane {
         });
 
         uefaButton = new Button("UEFA");
+        uefaButton.setTooltip(createToolTip("UEFA"));
         uefaButton.setOnMouseClicked(mouseEvent -> {
             table.getItems().clear();
             searchField.setText("");
@@ -59,6 +75,7 @@ public class QualifierPane extends BorderPane {
         });
 
         conmebolButton = new Button("CONMEBOL");
+        conmebolButton.setTooltip(createToolTip("CONMEBOL"));
         conmebolButton.setOnAction(mouseEvent -> {
             table.getItems().clear();
             searchField.setText("");
@@ -67,6 +84,7 @@ public class QualifierPane extends BorderPane {
         });
 
         concacafButton = new Button("CONCACAF");
+        concacafButton.setTooltip(createToolTip("CONCACAF"));
         concacafButton.setOnAction(mouseEvent -> {
             table.getItems().clear();
             searchField.setText("");
@@ -75,6 +93,7 @@ public class QualifierPane extends BorderPane {
         });
 
         cafButton = new Button("CAF");
+        cafButton.setTooltip(createToolTip("CAF"));
         cafButton.setOnAction(mouseEvent -> {
             table.getItems().clear();
             searchField.setText("");
@@ -83,6 +102,7 @@ public class QualifierPane extends BorderPane {
         });
 
         afcButton = new Button("AFC");
+        afcButton.setTooltip(createToolTip("AFC"));
         afcButton.setOnAction(mouseEvent -> {
             table.getItems().clear();
             searchField.setText("");
@@ -91,6 +111,7 @@ public class QualifierPane extends BorderPane {
         });
 
         ofcButton = new Button("OFC");
+        ofcButton.setTooltip(createToolTip("OFC"));
         ofcButton.setOnAction(mouseEvent -> {
             table.getItems().clear();
             searchField.setText("");
@@ -123,6 +144,19 @@ public class QualifierPane extends BorderPane {
                concacafButton, cafButton, afcButton, ofcButton, qualifiedButton, searchField,searchButton);
 
 
+    }
+
+    private Tooltip createToolTip(String confederation) {
+        Tooltip tooltip = new Tooltip();
+        tooltip.setText(confInfo.get(confederation));
+        try {
+            tooltip.setGraphic(new ImageView(
+                    this.getClass().getResource("Images/"+confederation+".png"
+                    ).toString()));
+        } catch (Exception e) {
+            tooltip.setText("Missing Logo!");
+        }
+        return tooltip;
     }
 
     private void createTable() {
