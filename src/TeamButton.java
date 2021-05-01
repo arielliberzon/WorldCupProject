ac98735b8d4f2972a0f1b3deb1e94d3bbc52f366
greenwolf5 <25jazz25@gmail.com>
2021-05-01 13:59:06 -0400

    Working on the right click left click interactions

--- src/TeamButton.java
+++ src/TeamButton.java
@@ -1,21 +1,3 @@
-<<<<<<< Updated upstream
-import javafx.scene.control.Button;
-
-public class TeamButton extends Button{
-    int xCord;
-    int yCord;
-
-    public TeamButton(){
-        super();
-        xCord = 0;
-        yCord = 0;
-    }
-
-    public TeamButton(String teamName, int xCord, int yCord){
-        super(teamName);
-        this.xCord = xCord;
-        this.yCord = yCord;
-=======
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 
@@ -45,6 +27,5 @@ public class TeamButton extends Button{
     }
     public Game getGame() {
         return game;
->>>>>>> Stashed changes
     }
 }
