1c887bfb7dcb9e7eb4c7158651dbfba2ab247adc
Emolyn18 <e_tumwebaze@massbay.edu>
2021-05-03 11:50:24 -0400

    Update TeamInfo.java

--- src/TeamInfo.java
+++ src/TeamInfo.java
@@ -1,22 +1,27 @@
+/**
+ * Programmer Emolyn Tumwebaze
+**/
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Locale;
 
-// TODO: Add authors! (first and last name add the top of the method)
-// TODO: Add comments and description
 public class TeamInfo {
-    private HashMap<String, Team> teams;
+    private HashMap<String, Team> teams; // creating a hashmap<String, Team>
 
+    // Default constructor
     public TeamInfo() {
         teams = new HashMap<>();
         loadFromFile();
     }
-
+    /**
+     * @author Emolyn Tumwebaze
+     * This method will load all the team information from the teamInfo.txt file via a BufferedReader
+     * into the teams HashMap.
+     *
+     */
     private void loadFromFile(){
-        String firstLine;
+        String firstLine; //creating variables to get specific data from the file
         int ranking;
         String country;
         double totalPoints;
@@ -28,7 +33,7 @@ public class TeamInfo {
         try{
             BufferedReader br = new BufferedReader(new FileReader(fileName));
 
-            while((firstLine = br.readLine()) != null){
+            while((firstLine = br.readLine()) != null){ //reads first line given that it is not blank.
                 ranking = Integer.parseInt(firstLine);
                 country = br.readLine();
                 totalPoints = Double.parseDouble(br.readLine());
@@ -40,25 +45,35 @@ public class TeamInfo {
                 Team newTeam = new Team(ranking, country, totalPoints, countryCode,
                         confederation);
 
-                br.readLine();
+                br.readLine(); //reads empty line between team info.
 
-                teams.put(newTeam.getCountryCode(), newTeam);
+                teams.put(newTeam.getCountryCode(), newTeam);  //maps team country code with respective team object
             }
 
             br.close();
 
         }
         catch(IOException e) {
-           System.out.println("Error file "+fileName +" could not be found");
+           System.out.println("Error "+ fileName +" could not be found");
            System.exit(1);
         }
 
     }
-
+    /**
+     *  @author Emolyn Tumwebaze
+     * This method will take a parameter of a country code and return the team object corresponding to it.
+     * @return team.get(countryCode)
+     */
     public Team getTeam(String countryCode){
         return teams.get(countryCode);
     }
 
+    /**
+     *  @author Emolyn Tumwebaze
+     *  This method is returning a hashmap of type team and string.
+     *  Whenever this method is called, it takes team and returns a string of team.
+     *  @return teams
+     */
     public HashMap<String, Team> getTeamMap() {
         return teams;
     }
